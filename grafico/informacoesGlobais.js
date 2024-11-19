90f9f36b94277bdfab446b575d4bbf8d3427cfe7
JuliaManu <106744116+JuliaManu@users.noreply.github.com>
2024-11-19 16:25:37 -0300

    Update informacoesGlobais.js

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -1,20 +1,10 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
-async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
-
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
-
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
-}
-
-vizualizarInformacoesGlobais()
+// informacoesGlobais.js
+const informacoesGlobais = {
+    nomeApp: "Comparação de Uso de Celulares",
+    descricao: "API para comparar o uso de celulares no mundo e no colégio",
+};
+// quantidadeUsuarios.js
+const quantidadeUsuarios = {
+    usuariosMundo: 5.5,  // bilhões de usuários no mundo
+    usuariosColegio: 350,  // número de estudantes no colégio
+};
