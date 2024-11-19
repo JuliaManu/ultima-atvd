b24af5b708cc140e6b3720c7898f100cad6f1862
JuliaManu <106744116+JuliaManu@users.noreply.github.com>
2024-11-19 16:26:09 -0300

    Update informacoesGlobais.js

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -3,8 +3,3 @@ const informacoesGlobais = {
     nomeApp: "Comparação de Uso de Celulares",
     descricao: "API para comparar o uso de celulares no mundo e no colégio",
 };
-// quantidadeUsuarios.js
-const quantidadeUsuarios = {
-    usuariosMundo: 5.5,  // bilhões de usuários no mundo
-    usuariosColegio: 350,  // número de estudantes no colégio
-};
