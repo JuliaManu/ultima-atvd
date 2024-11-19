ccafc480d870356690b56f08db30498420e8a740
JuliaManu <106744116+JuliaManu@users.noreply.github.com>
2024-11-19 16:25:05 -0300

    Update common.js

--- grafico/common.js
+++ grafico/common.js
@@ -1,30 +1,11 @@
-const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyValue(variavel)
+// common.js
+function fetchComparacao() {
+    fetch('http://localhost:3000/api/comparacao')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Dados de comparação:', data);
+        })
+        .catch(error => {
+            console.error('Erro ao carregar os dados da comparação:', error);
+        });
 }
-
-const tickConfig = {
-    color: getCSS('--primary-color'),
-    size: 16,
-    family: getCSS('--font')
-}
-
-function criarGrafico(data, layout) {
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    const config = {
-        responsive: true,
-        displayModeBar: false
-    }
-    Plotly.newPlot(grafico, data, layout, config)
-}
-
-function incluirTexto(texto) {
-    const container = document.getElementById('graficos-container')
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = texto
-    container.appendChild(paragrafo)
-}
-
-export { getCSS, tickConfig, criarGrafico, incluirTexto }
